ddadeb05eec10f97764ea9d03810f6851404a084
Clark Kampfe <clark.kampfe@gmail.com>
2013-09-18 12:44:35 -0500

    solved mergesort with explicit

--- mergesort.rb
+++ mergesort.rb
@@ -1,47 +1,41 @@
+# mergesort, invented by John von Neumann
+
 def divide(an_array)
-  # base
+  # base case
   if an_array.length <= 1
     return an_array
   end
 
-  left = an_array.select { |element| an_array.index(element) < an_array.length / 2 } 
-  # p left
-  right = an_array.select { |element| an_array.index(element) >= an_array.length / 2 }
-  # p right
-
+  left, right = [], []
+  # elements with indices < length / 2 go to left, all others go to right
+  an_array.each_with_index { |element, index| index < an_array.length / 2 ? left << element : right << element }
+  
+  # recurse to reduce all to bases 
   left = divide(left)
   right = divide(right)
-
+  
+  # merge them back together in order
   merge(left, right)
 end
 
 
 def merge(left_array, right_array)
   sorted = [] 
-
+  
   m = lambda do |left_array, right_array|
     if left_array.length == 0 && right_array.length == 0
       return sorted
-    elsif left_array.length == 0
+    elsif left_array.length < 1
       sorted << right_array.shift
       m.call(left_array, right_array)
-    elsif right_array.length == 0
+    elsif right_array.length < 1 
       sorted << left_array.shift
       m.call(left_array, right_array)
     else
-      left_array.first < right_array.first ? sorted << left_array.shift : sorted << right_array.shift
+      left_array.first <= right_array.first ? sorted << left_array.shift : sorted << right_array.shift
       m.call(left_array, right_array)
     end
   end
   
   m.call(left_array, right_array)
 end
-
-
-
-
-test_odd = [1,5,7,8,1,9,4,0,12]
-test_even = [1,2,5,6,8,3,12,10]
-
-p divide(test_even)
-p divide(test_odd)
