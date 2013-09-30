06e7f6c216bc4e29ab607c25722b0e2d118929a9
Clark Kampfe <clark.kampfe@gmail.com>
2013-09-30 14:40:46 -0500

    everything but unfound elements

--- binary_search.rb
+++ binary_search.rb
@@ -2,24 +2,20 @@
 
 def binary_search(n, list, place, adj)
 
-  if list[place] == n 
+  # need to implement `return -1 if not found`
+  
+  if list[place] == n
     return list.rindex(list[place]) + adj
   end
 
-  if list[place] < n
-    binary_search(n,   
-                  list[place..-1], # list slice
-                  (list[place..-1].length / 2), # place
-                  (adj + place)) # adjust
-  else
-    binary_search(n,
-                  list[0..place], # list slice
-                  (list[0..place].length / 2), # place
-                  adj)
-                  #list.length - place) # adjust
+  if list[0] == n  
+    return 0
+  elsif list[place] < n
+    return binary_search(n, list[place..-1], (list[place..-1].length / 2), (adj + place)) 
+  elsif list[place] > n
+    return binary_search(n, list[0..place], (list[0..place].length / 2), adj)
   end
 
-  # need to implement `return -1 if not found`
 
 end
 
@@ -27,5 +23,9 @@ my_list = [1,2,45,88,105,200,301,302,310,980]
 
 # works for everything but first element
 my_list.each do |x|
+  p "element = #{x}"
   p binary_search(x, my_list, my_list.length / 2, 0) 
 end
+
+
+p binary_search(90, my_list, my_list.length / 2, 0)
