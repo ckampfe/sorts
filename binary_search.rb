eab61dbe3388f208aeca00bee2e507ddc76bdd06
Clark Kampfe <clark.kampfe@gmail.com>
2013-09-30 14:46:05 -0500

    cleanup binary_search

--- binary_search.rb
+++ binary_search.rb
@@ -5,15 +5,15 @@ def binary_search(n, list, place, adj)
   # need to implement `return -1 if not found`
   
   if list[place] == n
-    return list.rindex(list[place]) + adj
+    return place + adj
   end
 
   if list[0] == n  
-    return 0
+    0
   elsif list[place] < n
-    return binary_search(n, list[place..-1], (list[place..-1].length / 2), (adj + place)) 
+    binary_search(n, list[place..-1], (list[place..-1].length / 2), (adj + place)) 
   elsif list[place] > n
-    return binary_search(n, list[0..place], (list[0..place].length / 2), adj)
+    binary_search(n, list[0..place], (list[0..place].length / 2), adj)
   end
 
 
@@ -21,11 +21,11 @@ end
 
 my_list = [1,2,45,88,105,200,301,302,310,980]
 
-# works for everything but first element
+# test
 my_list.each do |x|
   p "element = #{x}"
   p binary_search(x, my_list, my_list.length / 2, 0) 
 end
 
-
+# unfound test
 p binary_search(90, my_list, my_list.length / 2, 0)
