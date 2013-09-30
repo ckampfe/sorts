d83587c66150d2c9175803e36d1c1355df39625b
Clark Kampfe <clark.kampfe@gmail.com>
2013-09-30 14:03:14 -0500

    binary search works for everything but element 0 and unfound elements

--- binary_search.rb
+++ binary_search.rb
@@ -1,22 +1,31 @@
 # takes a sorted array
 
-def binary_search(n, list, place)
-  p list
+def binary_search(n, list, place, adj)
 
-  if list[place] == n
-    list.rindex(list[place])
-  elsif list[place] < n
-    p "place < n"
-    binary_search(n, list.slice(place, list.rindex(list[-1])), list.slice(place, list[-1]).length / 2)
-  else
-    p "place > n"
-    binary_search(n, list.slice(list.first, list.rindex(list[place])), list.slice(list.first, list[place]).length / 2)
+  if list[place] == n 
+    return list.rindex(list[place]) + adj
   end
 
+  if list[place] < n
+    binary_search(n,   
+                  list[place..-1], # list slice
+                  (list[place..-1].length / 2), # place
+                  (adj + place)) # adjust
+  else
+    binary_search(n,
+                  list[0..place], # list slice
+                  (list[0..place].length / 2), # place
+                  adj)
+                  #list.length - place) # adjust
+  end
+
+  # need to implement `return -1 if not found`
 
 end
 
 my_list = [1,2,45,88,105,200,301,302,310,980]
 
-p binary_search(105, my_list, my_list.length/2)
-
+# works for everything but first element
+my_list.each do |x|
+  p binary_search(x, my_list, my_list.length / 2, 0) 
+end
